a748b17f5a2b2aa7b8421265249c675f51ea4a7f
haseeb0089 <120918012+haseeb0089@users.noreply.github.com>
2024-04-28 23:44:46 +0500

    updated

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -1,7 +1,7 @@
 import mongoose, { ConnectOptions } from 'mongoose';
 
 export const dbConnection = (): void => {
-  const mongoURI = 'mongodb://127.0.0.1:27017/Ecommerce_24';
+  const mongoURI = 'mongodb://127.0.0.1:27017/Ecommerce_Backend';
 
   const options: ConnectOptions = {
     useNewUrlParser: true,
