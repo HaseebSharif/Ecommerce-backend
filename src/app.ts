c5d3711953e8a96bd9c06177677d3880ac6fe412
haseeb0089 <120918012+haseeb0089@users.noreply.github.com>
2024-06-11 15:37:05 +0500

    backend completed

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
 import express from "express";
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import NodeCache from "node-cache";
+import { config } from "dotenv";
 import morgan from "morgan";
+import Stripe from "stripe";
 import cors from "cors";
-import { dbConnection } from "./utils/features.js";
 
+// Importing Routes
+import userRoute from "./routes/user.js";
+import productRoute from "./routes/products.js";
+import orderRoute from "./routes/order.js";
+import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 
-//importing Routes
-import userRoutes from './routes/user.js'
+config({
+  path: "./.env",
+});
 
-const port = 4000;
-const mongoURI =  "mongodb://localhost:27017";
-// const stripeKey = process.env.STRIPE_KEY || "";
+const port = process.env.PORT || 4000;
+const mongoURI = process.env.MONGO_URI || "";
+const stripeKey = process.env.STRIPE_KEY || "";
 
-dbConnection();
+connectDB(mongoURI);
+
+export const stripe = new Stripe(stripeKey);
+export const myCache = new NodeCache();
 
 const app = express();
 
@@ -19,13 +33,20 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-app.get("/", (req , res ) => {
+app.get("/", (req, res) => {
   res.send("API Working with /api/v1");
 });
 
-//using routes
-app.use("/api/v1/user" , userRoutes )
+// Using Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
+
+app.use("/uploads", express.static("uploads"));
+app.use(errorMiddleware);
 
 app.listen(port, () => {
-    console.log(`Server is working on http://localhost:${port}`);
-  });
+  console.log(`Express is working on http://localhost:${port}`);
+});
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
 import { NewUserRequestBody } from "../types/types.js";
-// import { TryCatch } from "../middlewares/error.js";
-// import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "../middlewares/error.js";
+import ErrorHandler from "../utils/utility-class.js";
 
-export const newUser = (
+export const newUser = TryCatch(
   async (
     req: Request<{}, {}, NewUserRequestBody>,
     res: Response,
@@ -12,18 +12,18 @@ export const newUser = (
   ) => {
     const { name, email, photo, gender, _id, dob } = req.body;
 
-    
+    let user = await User.findById(_id);
 
-    // if (user)
-    //   return res.status(200).json({
-    //     success: true,
-    //     message: `Welcome, ${user.name}`,
-    //   });
+    if (user)
+      return res.status(200).json({
+        success: true,
+        message: `Welcome, ${user.name}`,
+      });
 
-    // if (!_id || !name || !email || !photo || !gender || !dob)
-    //   return next(new ErrorHandler("Please add all fields", 400));
+    if (!_id || !name || !email || !photo || !gender || !dob)
+      return next(new ErrorHandler("Please add all fields", 400));
 
-   let user = await User.create({
+    user = await User.create({
       name,
       email,
       photo,
@@ -38,3 +38,38 @@ export const newUser = (
     });
   }
 );
+
+export const getAllUsers = TryCatch(async (req, res, next) => {
+  const users = await User.find({});
+
+  return res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+export const getUser = TryCatch(async (req, res, next) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+  return res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const deleteUser = TryCatch(async (req, res, next) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+  await user.deleteOne();
+
+  return res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+});
